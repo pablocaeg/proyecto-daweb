b31b4a1db21ba12dc04a7fd00e05694ff0120127
Pablo Carrasco <pablocarrascoegea@gmail.com>
2023-04-22 19:35:45 +0200

    commit

--- react-express/react-project/src/components/Header/Header.js
+++ react-express/react-project/src/components/Header/Header.js
@@ -5,18 +5,13 @@ const Header = () => {
     <header>
         <img id="app-logo" src="logo-mifoodie.png" alt=""></img>
         <a id="app-title" href="#">MiFoodie</a>
-        <nav class="navbar navbar-expand-lg bg-info navbar-dark">
-       {/*Container wrapper*/}
-        
+        <nav class="navbar">        
           {/*Icons*/}
-        <ul class="navbar-nav navbar-nav ms-auto ">
-          <li class="">
-            <a href="#" tabindex="0" class="nav-link">Contacto</a>
-          </li>
-        </ul>
-      
-
-      {/*Container wrapper*/}
+          <ul class="navbar-nav navbar-nav ms-auto ">
+            <li class="">
+              <a href="#" tabindex="0" class="nav-link">Contacto</a>
+            </li>
+          </ul>
     </nav>
     </header>
   );
