84ee96c68caeeae9b5f6f0624dcea4b276e43517
Pablo Carrasco <pablo.carrasco@bemyvega.com>
2023-04-23 12:20:59 +0200

    quitada una palabra que sobraba

--- react-express/react-project/src/App.js
+++ react-express/react-project/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes, Link } from "react-router-dom"
+import { Route, Routes} from "react-router-dom"
 
 import React from "react";
 import Header from "./components/Header/Header";
