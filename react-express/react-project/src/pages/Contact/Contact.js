0053402331bbad3bac0c34e74ae378d91f71dae5
francordel <fran.j.cordel@gmail.com>
2023-04-23 18:29:29 +0200

    contacto text area

--- react-express/react-project/src/pages/Contact/Contact.js
+++ react-express/react-project/src/pages/Contact/Contact.js
@@ -5,9 +5,24 @@ import Footer from "../../components/Footer/Footer.js";
 const Contact = () => {
   return (
     <div className="contact-page">
-      <h1>Contact Us</h1>
-      <h3>Esto es un placeholder de la página de contacto</h3>
-      <h3>Pulsa en "MiFoodie" para volver a la página principal</h3>
+        <form class='contact-form'>
+          <h1>Contacto</h1>
+          <div class="validate-input" data-validate="Se requiere el nombre">
+            <input class="input" type="text" name="name" placeholder="Nombre"></input>
+          </div>
+          <div class="validate-input" data-validate="Se requiere un correo electrónico válido">
+            <input class="input" type="text" name="email" placeholder="Correo electrónico"></input>
+          </div>
+          <div class="validate-input" data-validate="Se requiere el asunto">
+            <input class="input" type="text" name="subject" placeholder="Asunto"></input>
+          </div>
+          <div class="validate-input " data-validate="Se requiere el mensaje">
+            <textarea class="message" type="text" name="message" placeholder="Mensaje"></textarea>
+          </div>
+          <div class="container-contact-form-btn">
+            <button class="contact-form-btn" type="submit">Enviar correo</button>
+          </div>
+        </form>
     </div>
   );
 };
