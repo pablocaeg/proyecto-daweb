05588847804c223a4a818b0cde441e55ee298f9d
Pablo Carrasco <pablocarrascoegea@gmail.com>
2023-04-26 08:27:31 +0200

    RestaurantList placeholder

--- react-express/react-project/src/components/RestaurantList/RestaurantList.js
+++ react-express/react-project/src/components/RestaurantList/RestaurantList.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const RestaurantList = () => {
+const RestaurantList = ({ restaurants }) => {
   return (
-    <>
-    </>
+    <div className="restaurant-list">
+      {restaurants.map((restaurant) => (
+        <div className="restaurant-item" key={restaurant.id}>
+          <h3 className="restaurant-name">{restaurant.name}</h3>
+          <p className="restaurant-info">Coordinates: {restaurant.coordinates}</p>
+          <p className="restaurant-info">Postal Code: {restaurant.postalCode}</p>
+          <p className="restaurant-info">Rating: {restaurant.rating}</p>
+        </div>
+      ))}
+    </div>
   );
 };
 
--- react-express/react-project/src/pages/Landing/Landing.js
+++ react-express/react-project/src/pages/Landing/Landing.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 import SearchBar from "../../components/SearchBar/SearchBar.js";
 import RestaurantList from "../../components/RestaurantList/RestaurantList.js";
+import restaurantsData from "../../data/restaurants.json";
 
 const Landing = () => {
   return (
     <>
       <SearchBar />
-      <RestaurantList />
+      <RestaurantList restaurants={restaurantsData} />
     </>
   );
 };
