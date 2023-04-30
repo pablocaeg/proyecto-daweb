4699354f8bea7e76891ac11b6b4a75b8509cc5ef
francordel <fran.j.cordel@gmail.com>
2023-04-30 17:55:12 +0200

    dos botones de seleccion de platos o menu y manejo estado

--- react-express/react-project/src/pages/Restaurante/Restaurante.js
+++ react-express/react-project/src/pages/Restaurante/Restaurante.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from "react";
+
 import { Route, Routes} from "react-router-dom"
 import { Link } from "react-router-dom"
 
@@ -6,19 +7,40 @@ import InfoRestaurante from "../../components/InfoRestaurante/InfoRestaurante.js
 import Platos from "../../components/Platos/Platos.js";
 
 const Restaurante = () => {
+  const [activeButton, setActiveButton] = useState("info");
+  
+// La función actualiza el estado activeButton utilizando setActiveButton con el valor del argumento button.
+  const handleClick = (button) => {
+    setActiveButton(button);
+  };
     return (
       <div className="restaurante-container" >
         <h1>Restaurante</h1>
         
         <h2>Platos</h2>
-        <div class = "restaurant-btn-container">
-          <div>
-          <Link id="link-inforestaurante" to="/restaurante/inforestaurante">Informacion</Link>
 
+        <div class="restaurant-btn-container">
+          <div
+          // Agrega una clase condicional al div de "Información". Si activeButton es igual a "info",
+          // entonces se agrega la clase "active", de lo contrario, no se agrega ninguna clase adicional.
+            class={`restaurant-btn-info ${activeButton === "info" ? "active" : ""}`}
+            // Esta línea asigna un controlador de eventos onClick al div de "Información".
+            // Cuando se hace clic en el div, se llama a la función handleClick con el argumento "info".
+            onClick={() => handleClick("info")}
+          >
+            <Link id="link-inforestaurante" to="/restaurante/inforestaurante">
+              Información
+            </Link>
           </div>
-          <div>
-          <Link id="link-platos" to="/restaurante/platos">Platos</Link>
-
+          <div
+            class={`restaurant-btn-platos ${
+              activeButton === "platos" ? "active" : ""
+            }`}
+            onClick={() => handleClick("platos")}
+          >
+            <Link id="link-platos" to="/restaurante/platos">
+              Platos
+            </Link>
           </div>
         </div>
         <Routes>
