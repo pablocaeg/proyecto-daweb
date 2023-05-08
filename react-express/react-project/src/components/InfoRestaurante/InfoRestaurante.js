4a9827fae78b413709719012ee5a6680770c6162
francordel <fran.j.cordel@gmail.com>
2023-05-08 17:42:45 +0200

    decoracion de info restaurante

--- react-express/react-project/src/components/InfoRestaurante/InfoRestaurante.js
+++ react-express/react-project/src/components/InfoRestaurante/InfoRestaurante.js
@@ -4,6 +4,59 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 const InfoRestaurante = ({ restaurant }) => {
   const [lat, lon] = restaurant.coordinates.split(', ').map(Number);
 
+  const renderRatingStars = (rating) => {
+    const maxRating = 5; // Set the maximum rating value here
+    const stars = [];
+    const roundedRating = Math.floor(rating);
+    const decimalPart = rating - roundedRating;
+
+    const starPath =
+      'M10 1.0001l2.247 6.954h7.303l-5.91 4.2999 2.247 6.954-5.887-4.2989-5.887 4.2989 2.247-6.954-5.91-4.2999h7.303z';
+
+    for (let i = 1; i < maxRating; i++) {
+      stars.push(
+        <svg
+          key={i}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          className="star"
+          fill={i <= roundedRating ? "#f5a623" : "#cccccc"}
+          stroke="#f5a623"
+          strokeWidth="1"
+          width="20"
+          height="20"
+        >
+          <path d={starPath}></path>
+        </svg>
+      );
+    }
+
+    if (decimalPart > 0) {
+      stars.push(
+        <svg
+          key={maxRating + 1}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          className="star"
+          width="20"
+          height="20"
+        >
+          <defs>
+            <clipPath id="starClip">
+              <rect x="0" y="0" width={decimalPart * 20} height="20" />
+            </clipPath>
+          </defs>
+          <g clipPath="url(#starClip)">
+            <path d={starPath} fill="#f5a623"></path>
+          </g>
+          <path d={starPath} fill="none" stroke="#f5a623" strokeWidth="1"></path>
+        </svg>
+      );
+    }
+
+    return stars;
+  };
+
   return (
     <div className="info-restaurante-container">
       <h1>Dónde estamos</h1>
@@ -21,10 +74,13 @@ const InfoRestaurante = ({ restaurant }) => {
         </MapContainer>
       </div>
       <p>Coordinates: {restaurant.coordinates} | Postal Code: {restaurant.postalCode}</p>
-      <p>Rating: {restaurant.rating}</p>
-      <h1>Incidencias</h1>
+      <h3>Rating: <span className="rating-stars">{renderRatingStars(restaurant.rating)} </span><span id="rating">({restaurant.rating})</span></h3>
+      <h1>Opiniones</h1>
     </div>
   );
 };
 
 export default InfoRestaurante;
+
+
+
