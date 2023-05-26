f5016aa952e7c40ab5b3951f48b867a6cd3e962c
Fran Cortés <100516685+francordel@users.noreply.github.com>
2023-05-26 11:19:04 +0200

    jwt

--- react-express/react-project/src/App.js
+++ react-express/react-project/src/App.js
@@ -11,18 +11,45 @@ const App = () => {
   const [restaurants, setRestaurants] = useState([]);
   const [restaurant, setRestaurant] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  function getCookie(name) {
+    // Inicialmente, se toma la cadena de cookies completa de document.cookie y se le añade un ; al principio.
+    // Esto se hace para que podamos buscar de manera consistente el nombre de la cookie precedido por un ; 
+    // garantizando así que no encontraremos nombres de cookies que sean parte de los valores de otras cookies.
+    const value = `; ${document.cookie}`;
+  
+    // A continuación, dividimos la cadena de cookies en el nombre de la cookie que estamos buscando.
+    // Si la cookie existe, terminaremos con una matriz de dos partes: todo antes de la cookie que estamos buscando
+    // y todo después de ella.
+    const parts = value.split(`; ${name}=`);
+  
+    // Si tenemos dos partes, entonces la cookie fue encontrada. En este caso, queremos devolver el valor de la cookie.
+    if (parts.length === 2) {
+      // Tomamos la segunda parte (todo después de la cookie que estamos buscando) y la dividimos por ; 
+      // Esto nos da una matriz de todo después de la cookie que estamos buscando, dividida en diferentes cookies.
+      // Como la cookie que estamos buscando es la primera en esta matriz, usamos shift() para obtenerla y devolverla.
+      return parts.pop().split(';').shift();
+    }
+  }
   
 
   const getRestaurants = async () => {
     try {
-      const response = await fetch('http://localhost:8090/restaurantes');
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+      const jwt = getCookie('jwt');
+      console.log('Cookie jwt: ', jwt);
+      if(!jwt) {
+        window.location.href = 'http://localhost:8090/restaurantes';
+      }else{
+        const response = await fetch('http://localhost:8090/restaurantes');
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }       
+        console.log('Response: ', response.data);
+        const data = await response.json();
+          console.log('Data received from server:', data.restaurante);
+        setRestaurants(data.restaurante);
+
       }
-    
-      const data = await response.json();
-        console.log('Data received from server:', data.restaurante);
-      setRestaurants(data.restaurante);
     } catch (error) {
       console.error('Error fetching restaurants data:', error);
     }
@@ -31,7 +58,7 @@ const App = () => {
   const getRestaurant = async (idrestaurante) => {
     try {
       setLoading(true);
-      const response = await fetch(`http://localhost:3000/api/restaurantes/${idrestaurante}`);
+      const response = await fetch(`http://localhost:8080/api/restaurantes/${idrestaurante}`);
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
